17ebaeb826eb4fdb928172b3128047c1344df1be
Rillieux <dave.kondris@gmail.com>
2021-01-30 20:50:29 +0100

    Initial Commit

---
+++ customBlurSafeAreas/customBlurSafeAreasApp.swift
@@ -0,0 +1,17 @@
+//
+//  customBlurSafeAreasApp.swift
+//  customBlurSafeAreas
+//
+//  Created by Dave Kondris on 30/01/21.
+//
+
+import SwiftUI
+
+@main
+struct customBlurSafeAreasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
